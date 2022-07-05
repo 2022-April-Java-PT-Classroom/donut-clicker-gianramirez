8b8aaa294db8c5f0f71e8156fc70ef2212dc2afe
gianramirez <radicgian@gmail.com>
2022-07-04 20:14:56 -0400

    start

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,17 +1,19 @@
 class DonutMaker {
 
-    constructor(numDonuts, numAutoclickers, autoclickerCost){
+    constructor(numDonuts, numAutoclickers, autoclickerCost, numMultipliers, clickCount){
         this.numDonuts = numDonuts;
         this.numAutoclickers = numAutoclickers;
         this.autoclickerCost = autoclickerCost;
+        this.numMultipliers = this.numMultipliers;
+        this.clickCount = this.clickCount;
     }
 
     addDonut() {
         this.numDonuts += 1;
 
-        if(this.numAutoclickers >= 1){
-            this.numDonuts++;
-        }
+        // if(this.numAutoclickers >= 1){
+        //     this.numDonuts++;
+        // }
         
     }
 
@@ -24,7 +26,11 @@ class DonutMaker {
         if(this.numAutoclickers >=1){
             this.autoclickerCost += this.autoclickerCost * 0.1
         }
-    }    
+    } 
+    
+    addMultiplier() {
+        this.numMultipliers 
+    }
 
 }
 
