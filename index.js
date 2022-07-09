143d48055ea376478a2964abc680daa2ad75575d
gianramirez <radicgian@gmail.com>
2022-07-08 23:13:55 -0400

    added cost visuals

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const buttonDonut = document.querySelector('#buttonDonut');
 const donutsMade = document.querySelector('#donutsMade');
 const autoclickerBtn = document.querySelector('#autoclickerBtn');
 const clickerspurchased = document.querySelector('#clickerspurchased');
-const clickerCost = document.querySelector('#clickerCost')
-const multiplierBtn = document.querySelector('#multiplierBtn')
+const clickerCost = document.querySelector('#clickerCost');
+const multiplierBtn = document.querySelector('#multiplierBtn');
 const multiplierspurchased = document.querySelector('#multiplierspurchased');
+const multiplierCost = document.querySelector('#multiplierCost');
 const MkDonuts = new DonutMaker(0, 0, 100, 0, 10, 1);
 
 buttonDonut.addEventListener('click', ()=> {
@@ -23,4 +24,5 @@ autoclickerBtn.addEventListener('click', ()=> {
 multiplierBtn.addEventListener('click', ()=> {
   MkDonuts.addMultiplier();
   multiplierspurchased.innerText = 'Multipliers: ' + MkDonuts.numMultipliers;
+  multiplierCost.innerText = 'Multiplier Cost: ' + MkDonuts.multipliersCost;
 })
